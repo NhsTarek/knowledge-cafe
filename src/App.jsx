326938e53ef89f77645d959eabaeb9ceb7600213
NhsTarek <htarek275@gmail.com>
2024-03-17 04:03:24 +0600

    fake data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Bookmarks from './Components/Bookmarks/Bookmarks'
+import Header from './Components/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-     
-      <h1 className='text-6xl'>Knowledge cafe</h1>
-      
-      
+
+      <Header></Header>
+      <div className='md:flex'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
+
+
     </>
   )
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import profile from '../../assets/images/profile.png'
+
+const Header = () => {
+    return (
+        <header className='flex justify-between p-4 mx-4 border-b-2'>
+            <h3 className="text-3xl font-bold">Knowledge cafe</h3>
+            <img src={profile}></img>
+        </header>
+    );
+};
+
+export default Header;
